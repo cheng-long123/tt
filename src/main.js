5d5113698086741880378ffc3ba37b855f03dc9b
cheng-long <1760016317@qq.com>
2020-04-26 23:41:11 +0800

    优化了 富文本编辑器 英文改中文

--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import router from './router'
 
 // 加载 element 组件库
 import ElementUI from 'element-ui'
-
+// 引进element-tiptap组件库
+import { ElementTiptapPlugin } from 'element-tiptap'
 // 加载 element 组件库样式
 import 'element-ui/lib/theme-chalk/index.css'
 
 // 加载全局样式
 // import './style/index.less'
-
+// 声明语言
+Vue.use(ElementTiptapPlugin, {
+  lang: 'zh'// 中文简体
+})
 // 注册ElementUI
 Vue.use(ElementUI)
 Vue.config.productionTip = false
